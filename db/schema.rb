e28f2d69c9eacc2ad44bb467244355bcc2a25ef0
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-25 00:29:44 -0500

    Added the field 'available' to the books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_044309) do
+ActiveRecord::Schema.define(version: 2020_11_25_052844) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "available"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+NUM_OF_SEEDS = 100
+
+NUM_OF_SEEDS.times do |i|
+  Book.create(
+    available: true,
+    title: Faker::Books::CultureSeries.book
+  )
+  puts "Inserted # #{i} seed into the database"
+end
